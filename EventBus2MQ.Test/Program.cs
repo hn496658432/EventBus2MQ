be99f85b3bd75b481dd6b26431044681f177cba5
Kl <hn496658432@163.com>
2021-08-21 23:08:09 +0800

    test

--- EventBus2MQ.Test/Program.cs
+++ EventBus2MQ.Test/Program.cs
@@ -1,9 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using EventBus2MQ;
+using System;
+using System.Threading.Tasks;
 
 //MqClient mqClient = new MqClient();
+class Program
+{
+    static async Task Main(string[] args)
+    {
 
-await MqClient.StartupAsync();
+        await MqClient.StartupAsync();
 
-Console.ReadLine();
+        Console.ReadLine();
+    }
+
+}
+
